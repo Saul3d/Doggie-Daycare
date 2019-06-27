4a9c9d05eac75ea158e16a18708c3a036de87331
Saul Solano <saul3d@yahoo.com>
2019-06-27 17:13:09 -0500

    completed setup

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>This is a test</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
