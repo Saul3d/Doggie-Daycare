9e0024208cfdf2547d8876cb004db5bb3abbd2d7
Saul Solano <saul3d@yahoo.com>
2019-06-29 16:32:44 -0500

    created staff component

---
+++ src/App/staff.js
@@ -0,0 +1,35 @@
+const staff = [
+  {
+    id: 'staff1',
+    name: 'Peppa Pig',
+    age: 8,
+    imageUrl: 'https://www.peppapig.com/wp-content/uploads/sites/3/2019/02/peppa_pig_splat.png',
+    
+  },
+  {
+    id: 'staff2',
+    name: 'Sussy Sheep',
+    age: 8,
+    imageUrl: 'https://www.peppapig.com/wp-content/uploads/sites/3/2019/02/suzy_sheep_splat.png',
+  },
+  {
+    id: 'staff3',
+    name: 'Pedro Pony',
+    age: 8,
+    imageUrl: 'https://www.peppapig.com/wp-content/uploads/sites/3/2019/02/pedro_pony_splat.png',
+  },
+  {
+    id: 'staff4',
+    name: 'George Pig',
+    age: 5,
+    imageUrl: 'https://www.peppapig.com/wp-content/uploads/sites/3/2019/02/george_pig_splat.png',
+  },
+  {
+    id: 'staff5',
+    name: 'Rebbeca Rabbit',
+    age: 9,
+    imageUrl: 'https://www.peppapig.com/wp-content/uploads/sites/3/2019/02/rebecca_rabbit_splat.png',
+  }
+];
+
+  export default staff;
